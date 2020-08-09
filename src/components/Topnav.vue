cc372a453424f4863f0c654fe73848d7b5f626ab
xhrbtk <xhtbtk@gmail.com>
2020-08-09 16:04:18 +0800

    feat:实现aside切换

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="topnav">
-        <div class="logo">LOGO</div>
+        <div class="logo" @click="toggleMenu">LOGO</div>
         <ul class="menu">
             <li>菜单1</li>
             <li>菜单2</li>
@@ -8,7 +8,17 @@
     </div>
 </template>
 <script lang="ts">
-export default {}
+import { Ref, inject } from 'vue'
+export default {
+    setup() {
+        const menuVisible = inject<Ref>('xxx')
+        console.log('topnav 获取的 menuVisible 为：' + menuVisible.value)
+        const toggleMenu = () => {
+            menuVisible.value = !menuVisible.value
+        }
+        return { toggleMenu }
+    },
+}
 </script>
 <style lang="scss" scoped>
 .topnav {
