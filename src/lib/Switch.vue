5cd518ef0a60447b40595280e2ea62a8a649b6f9
xhrbtk <xhtbtk@gmail.com>
2020-08-12 19:19:33 +0800

    feat:动画流畅

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -34,11 +34,15 @@ span {
     width: $h2;
     background: white;
     border-radius: $h2 / 2;
+    transition: left 0.25s;
 }
 button.checked {
     background-color: blue;
 }
 button.checked > span {
     left: calc(100% - 20px);
+}
+button:focus {
+    outline: none;
 }
 </style> 
