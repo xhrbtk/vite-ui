24fa401b5e4e963a91705d19d1ad93ec72d0bf25
xu.hongrui <xu.hongrui@uneed.com>
2020-08-13 19:19:40 +0800

    feat:创建一个可以用的button

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,19 @@
 <template>
     <div>
-        我是buttondemo
+        Button 示例
+    </div>
+    <h1>示例 1</h1>
+    <div>
+        <Button>你好</Button>
     </div>
 </template>
+
+<script lang="ts">
+import Button from '../lib/Button.vue'
+import { ref } from 'vue'
+export default {
+    components: { Button },
+    setup() {
+    },
+}
+</script>
